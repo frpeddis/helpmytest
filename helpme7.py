10ea8cdfa9a6cfca591ff6e52693af6d916144b3
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-30 22:53:50 +0200

    Update helpme7.py

--- helpme7.py
+++ helpme7.py
@@ -4,7 +4,7 @@ import pytesseract
 import openai
 
 # Initialize GPT API
-openai.api_key = st.secrets["API_KEY"]
+openai.api_key = "your-openai-api-key"
 
 # Set Tesseract path
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
@@ -22,15 +22,16 @@ lang_code = 'eng' if language == 'English' else 'ita'
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    rotated_image = ImageOps.exif_transpose(image).rotate(st.session_state.rotation)
 
-    st.image(rotated_image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button('Rotate'):
         st.session_state.rotation = (st.session_state.rotation + 90) % 360
 
-    
     if st.button('Perform OCR'):
+        # Apply the rotation right before OCR
+        rotated_image = ImageOps.exif_transpose(image).rotate(st.session_state.rotation)
+        
         st.write("Recognized Text")
         # Perform OCR based on selected language
         text = pytesseract.image_to_string(rotated_image, lang=lang_code)

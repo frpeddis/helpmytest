2b7cbc51ed1da4418231dbbde27ddd22c25685b2
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-31 22:07:56 +0200

    Update helpme9.py

--- helpme9.py
+++ helpme9.py
@@ -27,12 +27,22 @@ if uploaded_file is not None:
 
     # Analyze text using ChatGPT and provide an opinion
     if st.button('Analyze with ChatGPT'):
-        prompt = f"This is a text to analyze: {text}. First think step by step, then understand what is the topic of the text. You are an expert on that topic. Now give me your opinion about that topic. Then it is very important to look for any questions contained in the text. If you find a question, a quiz, a multichoise questions, etc give me the answer you consider correct to that question, that quiz, that multiple choise question. Do not end your output without giving a answer to questions contained in the text"
+        prompt = f"This is a text to analyze: {text}. First think step by step, then understand what is the topic of the text. You are an expert on that topic. Now give me your opinion about that topic."
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=prompt,
-            max_tokens=400,
+            max_tokens=200,
             temperature=0.2  # Lower temperature means less randomness
+        prompt = f"Now look for any questions contained in the text {text}. If you find a question, a quiz, a multichoise questions, etc give me the answer you consider correct to that question, that quiz, that multiple choise question. Do not end your output without giving a answer to questions contained in the text"
+                response = openai.Completion.create(
+                    engine="text-davinci-002",
+                    prompt=prompt,
+                    max_tokens=300,
+                    temperature=0.2  # Lower temperature means less randomness
+        
+        
+        
+                    
         )
 
         st.write("GPT-3 Analysis")

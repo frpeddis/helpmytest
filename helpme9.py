2ceaeecbf221b1ba151396e026d3994d969ef648
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-31 21:46:31 +0200

    Update helpme9.py

--- helpme9.py
+++ helpme9.py
@@ -10,8 +10,8 @@ openai.api_key = st.secrets["API_KEY"]
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 # Initialize Streamlit session state
-if 'rotation' not in st.session_state:
-    st.session_state.rotation = 0
+if 'image' not in st.session_state:
+    st.session_state.image = None
 
 if 'crop_coords' not in st.session_state:
     st.session_state.crop_coords = (0, 0, 100, 100)
@@ -24,31 +24,32 @@ language = st.selectbox('Select OCR Language', ['English', 'Italian'])
 lang_code = 'eng' if language == 'English' else 'ita'
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert('RGB')
+    st.session_state.image = Image.open(uploaded_file).convert('RGB')
 
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+if st.session_state.image is not None:
+    st.image(st.session_state.image, caption="Uploaded Image", use_column_width=True)
 
     if st.button('Rotate'):
-        st.session_state.rotation = (st.session_state.rotation + 90) % 360
-        image = image.transpose(Image.Transpose.ROTATE_90)  # Rotate 90 degrees
-        st.session_state.crop_coords = (0, 0, image.size[0], image.size[1])  # Update crop coordinates to match new dimensions
-        st.image(image, caption="Rotated Image", use_column_width=True)
+        rotated_image = st.session_state.image.transpose(Image.Transpose.ROTATE_90)  # Rotate 90 degrees
+        st.session_state.image = rotated_image  # Update session state
+        st.session_state.crop_coords = (0, 0, rotated_image.size[0], rotated_image.size[1])  # Update crop coordinates
+        st.image(rotated_image, caption="Rotated Image", use_column_width=True)
 
     x1, y1, x2, y2 = st.session_state.crop_coords
-    x1 = st.slider("Left (x1)", min_value=0, max_value=image.size[0]-1, value=x1)
-    y1 = st.slider("Top (y1)", min_value=0, max_value=image.size[1]-1, value=y1)
-    x2 = st.slider("Right (x2)", min_value=0, max_value=image.size[0], value=x2)
-    y2 = st.slider("Bottom (y2)", min_value=0, max_value=image.size[1], value=y2)
+    x1 = st.slider("Left (x1)", min_value=0, max_value=st.session_state.image.size[0]-1, value=x1)
+    y1 = st.slider("Top (y1)", min_value=0, max_value=st.session_state.image.size[1]-1, value=y1)
+    x2 = st.slider("Right (x2)", min_value=0, max_value=st.session_state.image.size[0], value=x2)
+    y2 = st.slider("Bottom (y2)", min_value=0, max_value=st.session_state.image.size[1], value=y2)
 
     if st.button('Crop'):
         st.session_state.crop_coords = (x1, y1, x2, y2)
-        image = image.crop(st.session_state.crop_coords)
-        st.image(image, caption="Cropped Image", use_column_width=True)
+        cropped_image = st.session_state.image.crop(st.session_state.crop_coords)
+        st.session_state.image = cropped_image  # Update session state
+        st.image(cropped_image, caption="Cropped Image", use_column_width=True)
 
     if st.button('Perform OCR'):
-        rotated_image = image  # Image has been rotated or cropped above, if requested
         st.write("Recognized Text")
-        text = pytesseract.image_to_string(rotated_image, lang=lang_code)
+        text = pytesseract.image_to_string(st.session_state.image, lang=lang_code)
         st.write(text)
 
         if st.button('Analyze'):
